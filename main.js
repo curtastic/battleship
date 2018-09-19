e66d0b17fe44fc3b54a21e9394477e62906bb136
Curtis Robinson <curtastic@gmail.com>
2018-09-19 11:58:38 -0700

    Added player grids with display.

--- main.js
+++ main.js
@@ -8,9 +8,19 @@ const canvasSizeRatio = canvasWidth / canvasHeight;
 
 class Game {
 	constructor() {
+		this.canvas = null;
+		this.context = null;
+		this.players = [];
+		this.you = null;
+		this.enemy = null;
+		this.waterImage = new Image();
+		this.waterImage.src = 'images/water.png';
 	}
 	
 	init() {
+		this.you = new Player("You");
+		this.enemy = new Player("Opponent");
+		
 		this.canvas = mainCanvas;
 		this.canvas.setAttribute('width', canvasWidth);
 		this.canvas.setAttribute('height', canvasHeight);
@@ -36,10 +46,71 @@ class Game {
 	draw() {
 		this.context.fillStyle = "#89A";
 		this.context.fillRect(0,0,canvasWidth,canvasHeight);
+		for(var player of this.players)
+		{
+			this.drawGrid(player);
+		}
+	}
+	
+	drawGrid(player) {
+		this.context.save();
+		this.context.translate(player.boardX, player.boardY);
+		
+		// Draw grid background.
+		for(var y = 0; y < gridLength; y++) {
+			for(var x = 0; x < gridLength; x++) {
+				this.context.drawImage(this.waterImage, x * tileSize, y * tileSize);
+			}
+		}
+		
+		// Draw grid labels.
+		this.context.fillStyle = '#000';
+		this.context.textAlign = 'center';
+		this.context.textBaseline = 'middle';
+		const charCodeA = "A".charCodeAt(0);
+		for(var y = 0; y < gridLength; y++) {
+			this.context.fillText(String.fromCharCode(charCodeA + y), -tileSize / 2, (y + .5) * tileSize);
+		}
+		for(var x = 0; x < gridLength; x++) {
+			this.context.fillText(x, (x + .5) * tileSize, -tileSize / 2);
+		}
+		
+		// Draw grid lines.
+		for(var y = 0; y <= gridLength; y++) {
+			this.context.fillRect(0, y * tileSize, gridLength * tileSize, 1);
+		}
+		for(var x = 0; x <= gridLength; x++) {
+			this.context.fillRect(x * tileSize, 0, 1, gridLength * tileSize);
+		}
+		
+		this.context.restore();
 	}
 }
 
-var game = new Game();
+class Player {
+	constructor(name) {
+		this.name = name;
+		
+		// Set the player's board position.
+		this.boardX = tileSize;
+		this.boardY = tileSize;
+		if(game.players.length > 0) {
+			this.boardX += (gridLength + 1) * tileSize;
+		}
+		
+		// Create the player's grid.
+		this.grid = [];
+		for(var x = 0; x < gridLength; x++) {
+			this.grid[x] = [];
+			for(var y = 0; y < gridLength; y++) {
+				this.grid[x][y] = 0;
+			}
+		}
+		game.players.push(this);
+	}
+}
+
+let game = new Game();
 
 window.onload = () => {
 	game.init();
